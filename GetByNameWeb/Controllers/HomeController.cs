070098dedffcad16f74454aeab6581375b68c7be
soramusoka <soramusoka@gmail.com>
2013-07-01 22:16:36 +0400

    start project

---
+++ GetByNameWeb/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using GetByNameLibrary.Domains;
+using SerializeLibra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GetByNameWeb.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			var list = new JsonSerializer().Load<List<TwitterEntry>>(@"tweets.json");
+
+			ViewBag.Entries = list;
+
+			return View();
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 GetByNameWeb
 ============
+VisualStudio 2012 solution
 
 GetByName WebSite
